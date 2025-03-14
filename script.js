4139a63eee847f02595cef261add3c772bc75708
davyd <davydedivaldo@gmail.com>
2025-03-13 22:56:15 -0300

    .

---
+++ script.js
@@ -0,0 +1,8 @@
+$(window).on('scroll', function(){
+    const header = $('header');
+    const scrollPosition = $(window).scrollTop()- header.outerHeight();
+
+    if(scrollPosition > 0){
+        
+    }
+});
